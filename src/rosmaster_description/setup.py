992047d64dc5af3c77c21140f35e276eda980a48
dinav2 <wealthydina@icloud.com>
2025-05-30 05:48:16 +0000

    Added dependencies and data files

--- src/rosmaster_description/setup.py
+++ src/rosmaster_description/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'rosmaster_description'
 
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+	(os.path.join('share',package_name,'launch'),glob(os.path.join('launch','*launch.py'))),
+	(os.path.join('share',package_name,'urdf'),glob(os.path.join('urdf','*.*'))),
+	(os.path.join('share',package_name,'meshes'),glob(os.path.join('meshes','*.*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
