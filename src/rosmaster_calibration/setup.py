17ed6a7d9c79b886bb913c3d2df940eadc02fe27
David Gómez Carrillo <wealthydina@icloud.com>
2025-06-12 12:58:16 -0600

    Update setup.py

--- src/rosmaster_calibration/setup.py
+++ src/rosmaster_calibration/setup.py
@@ -7,7 +7,8 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/auto_calibration.launch.py']),
     ],
